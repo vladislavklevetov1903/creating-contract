5a93bad49bbc60424ba002d7ba98c6dfb6bbc5b1
vladislavklevetov1903 <kvd1131@gmail.com>
2025-02-16 19:53:10 +0300

    Initial commit

---
+++ src/main/java/com/creating_contract/CreatingContractApplication.java
@@ -0,0 +1,13 @@
+package com.creating_contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creating_contract/CreatingContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creating_contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
